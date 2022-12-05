a83f3c53db7ba50530afba7d5471dd873a6707eb
Felipe Ozias de Oliveira <oziasfelipe38@gmail.com>
2022-12-05 10:13:08 -0300

    feat: new project
    create project 02.

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    loop {
+        let mut terminal1 = Terminal::new();
+        let todo = terminal1.ask_for_new_todo();
+        terminal1.show_todo(&todo);
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Todo {
+    message: String,
+}
+
+impl Todo {
+    fn new(message: String) -> Self {
+        Todo { message }
+    }
+}
+
+use std::io::{ Stdin, Stdout, Write };
+
+struct Terminal {
+    stdin: Stdin,
+    stdout: Stdout,
+}
+
+impl Terminal {
+    fn new() -> Self {
+        Terminal {
+            stdin: std::io::stdin(),
+            stdout: std::io::stdout(),
+        }
+    }
+
+    fn ask_for_new_todo(&mut self) -> Todo {
+
+        println!("🖋  Você deseja adicionar um novo TODO? (Responta do s ou n)");
+        let mut buf = String::new();
+        self.stdin.read_line(&mut buf).unwrap();
+
+        if buf.trim().to_string().to_lowercase() == "n" {
+            std::process::exit(0);
+        } else {
+            let mut todo_add = String::new();
+            println!("Digite o seu TODO abaixo: ⤵ ");
+            self.stdin.read_line(&mut todo_add).unwrap();
+            Todo::new(todo_add.trim().to_string())
+        }
+    }
+
+    fn show_todo(&mut self, todo: &Todo) {
+        println!("----------------------------------------------------");
+        writeln!(self.stdout, "✅ 🟢 O TODO adicionado foi: '{}' 🟢", todo.message).unwrap();
+        println!("----------------------------------------------------");
+    }
+}
